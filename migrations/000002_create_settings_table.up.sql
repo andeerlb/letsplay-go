c67f15fb55c190c3116438b5d7298a12259c0099
Anderson Babinski <andeerlb@gmail.com>
2025-07-30 01:12:25 -0300

    migrations

---
+++ migrations/000002_create_settings_table.up.sql
@@ -0,0 +1,5 @@
+CREATE TABLE settings (
+                          user_id UUID PRIMARY KEY,
+                          layout VARCHAR(255) NOT NULL,
+                          language VARCHAR(255) NOT NULL
+);
